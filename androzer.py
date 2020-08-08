b8b4b0763daf6c2dc38f1b4b1b43cfe566e96585
az0mb13 <qwertyuiop.dixit3@gmail.com>
2020-08-08 12:52:23 +0530

    Completed mainframe

--- androzer.py
+++ androzer.py
@@ -1,16 +1,35 @@
 import subprocess
+from subprocess import PIPE
 import os
 
 portfw = subprocess.run(["adb", "forward", "tcp:31415", "tcp:31415"], stdout=subprocess.DEVNULL)
 
+def gen_report():
+    print()
+
 def runall_scans(apkname):
-    commands = ['app.activity.info -a', 'app.package.attacksurface', 'app.package.launchintent']
-    #console connect
+
+    #Creating results directory if not exists
+    if os.path.isdir(apkname + '_results') == False:
+        os.mkdir(apkname + '_results')
+
+    #Speficy commands to run here    
+    commands = [
+        'app.activity.info -a', 'app.package.attacksurface', 'app.package.launchintent'
+        ]
+    dconcon = 'drozer console connect -c "run ' #Initiating command
+
+    #Running them in a loop
     for command in commands:
-        os.system('drozer console connect -c "run ' + command + " " + apkname + '"')
+        runme = subprocess.Popen([dconcon + command + ' ' + apkname + '"'], stdout=PIPE, universal_newlines=True, shell=True)
+        (output, err) = runme.communicate()
+        runme.wait()
+        print(output)
+
+        #Report generation
+        gen_report()
 
 def main():
-    print('test')
     runall_scans('com.mwr.example.sieve')
 
 if __name__ == "__main__":
